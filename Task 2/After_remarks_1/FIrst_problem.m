b41322563f5c237d530fc5f6dbe7ef9ce066dea7
xmickos <91094084+xmickos@users.noreply.github.com>
2023-11-09 21:03:34 +0300

    Update FIrst_problem.m

--- Task 2/After_remarks_1/FIrst_problem.m
+++ Task 2/After_remarks_1/FIrst_problem.m
@@ -1,103 +1,58 @@
-clear all; clc; close all;
-[data, Fs] = audioread("/Users/xmickos/Desktop/MIPT/КМТТ/task/task3.wav");
-L = length(data);
+clear all;
+clc;
+close all;
 
-% Перезапись с выбором каждого второго отcчёта 
+[data, Fs] = audioread("/MATLAB Drive/KMTT_Task2/task/file2.wav");
+data = data.'; 
+L = 2000;
 
-data_lower = ones(round(L/2), 2);
-for i = 1:round(L/2) - 1
-    data_lower(i, :) = data(2*i, :);
-end
+% Спектр считанного сигнала
+y_freqs = fft(data);
 
-% Длительность аудиофайла осталась прежней - 34 секунды.
+% Поскольку спектр возвращается симметричный, искать максимумы будем
+% только в первой половине.
 
-audiowrite("/Users/xmickos/Desktop/MIPT/КМТТ/Task 3 - Audio 1/lower_frequency.wav", data_lower, Fs/2);
+[argmaxs, maxs] = maxk(y_freqs(1:end/2), 3);
+fprintf("Three main harmonics are: %3.f Hz," + ...
+    " %3.f Hz and %3.f Hz\n", maxs(1), maxs(2), maxs(3));
 
-fprintf("После понижения частоты дискретизации сигнал стал на слух более" + ...
-    " приглушённым, высокие звуки стали звучать ниже.\n")
+% Отфильтруем сигнал, исключив из него все гармоники кроме трёх искомых:
 
-data_higher = ones(2*L, 2);
-for i = 1:L
-    data_higher(2*i, :) = data(i, :);
-end
-
-data_higher(1, :) = data_higher(2, :) / 2;
-for i = 3:L - 1
-    data_higher(i, :) = (data_higher(i - 1, :) + data_higher(i + 1, :)) / 2;
-    i = i + 1;
-end
-
-audiowrite("/Users/xmickos/Desktop/MIPT/КМТТ/Task 3 - Audio 1/higher_frequency.wav", data_higher, 2*Fs);
-
-fprintf("После увеличения частоты дискретизации сигнал" + ...
-    "на слух стал более 'звонким', высокие звуки стали" + ...
-    "слышаться чётче и ярче, в то время как \nнизкие," + ...
-    "наоборот, оказались приглушены.\n");
-
-% Вычисление спектров трёх исследуемых сигналов
-
-P_data = calculatePower(data);
-P_lower_data = calculatePower(data_lower);
-P_higher_data = calculatePower(data_higher);
-
-data = data / P_data;
-data_lower = data_lower / P_lower_data;
-data_higher = data_higher / P_higher_data;
-
-freq_data = fft(data);
-freq_lower_data = fft(data_lower);
-freq_higher_data = fft(data_higher);
+filtered_y = zeros(1, Fs, "double");
+filtered_y(maxs) = y_freqs(maxs);
+filtered_y(Fs - maxs + 1) = y_freqs(maxs);
 
 
+% Сравнение спектров изначального и отфильтрованного сигналов
 figure;
 subplot(2, 1, 1)
-plot(abs(freq_data(1:end/2)));
-title("Amplitude Spectrum of a given signal.");
+plot(abs(y_freqs(1:end/2)));
+title("Unfiltered signal spectrum.");
 xlabel("f (Hz)")
+ylabel("Amplitude")
 
 subplot(2, 1, 2)
-plot(10*log10(abs(freq_data(1:end/2))));
-title("Logarithm scale spectrum of a given signal.");
-xlabel("f (Hz)")
-
-savefig("/Users/xmickos/Desktop/MIPT/КМТТ/Task 3 - Audio 1/spectrum_initial.fig")
-
-
-figure;
-subplot(2, 1, 1)
-plot(abs(freq_lower_data(1:end/2)));
-title("Amplitude Spectrum of a downsampled signal");
-xlabel("f (Hz)")
-
-subplot(2, 1, 2)
-plot(10*log10(abs(freq_lower_data(1:end/2))));
-title("Logarithm scale spectrum of a downsampled signal.");
+plot(abs(filtered_y(1:end/2)))
+title("Filtered signal spectrum.");
 xlabel("f (Hz)");
-savefig("/Users/xmickos/Desktop/MIPT/КМТТ/Task 3 - Audio 1/spectrum_downsampled.fig")
+ylabel("Amplitude");
+
+filtered_signal = ifft(filtered_y);
+
+% Сравнение графиков изначального и отфильтрованного сигналов
 
 figure;
 subplot(2, 1, 1)
-plot(abs(freq_higher_data(1:end/2)));
-title("Amplitude Spectrum of signal with increased frequency rate");
+plot(data)
+title("Unfiltered signal in time domain")
 xlabel("f (Hz)")
+ylabel("Amplitude");
+ylim([-1.5 1.5])
 
 subplot(2, 1, 2)
-plot(10*log10(abs(freq_higher_data(1:end/2))))
-title("Logarithm spectrum of a upsampled signal.");
-savefig("/Users/xmickos/Desktop/MIPT/КМТТ/Task 3 - Audio 1/spectrum_upsampled.fig")
-
-
-fprintf("Как можно видеть, при увеличении частоты дискретизации спектр сигнала сжимается вдвое по оси частоты," + ...
-    "это позволяет обнаружить более \nвысокие частоты т.к. спектр сигнала расширяется.\nУменьшение же частоты дискрети" + ...
-    "зации вдвое приводит к 'растяжению' спектра в два раза, что может привести к потере высокочастотной информации.\n")
-fprintf("Cледовательно, в случае увеличения частоты дискретизации полученный спектр совпадает с исходным в первой половине.\n" + ...
-    "В случае с уменьшением – первая половина спектра изначального сигнала совпадает со спектром полученного после" + ...
-    "уменьшения частоты\n дискретизации.\n");
-
-
-function power = calculatePower(signal)
-    power = sum(signal.^2) / length(signal);
-end
-
-
+plot(abs(filtered_signal(1:end/2)))
+title("Filtered signal in time domain.");
+xlabel("f (Hz)");
+ylabel("Amplitude");
+ylim([-1.5 1.5])
 
