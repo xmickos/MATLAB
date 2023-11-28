12dd2d5f2dbe23e470fcae81c573711506a299b9
xmickos <91094084+xmickos@users.noreply.github.com>
2023-11-28 22:47:14 +0300

    Second try without any additional tasks.

---
+++ CalcLength.m
@@ -0,0 +1,4 @@
+function distance = CalcLength(vec)
+%     distance = sqrt(sum(vec .* vec));
+    distance = abs(vec);
+end
---
+++ Noise.m
@@ -0,0 +1,17 @@
+function NoisedSignal = Noise(Signal, SNR)
+    % генерация белого шума с мощностью, заданной в SNR
+
+    % вычисление мощности сигнала
+    Psignal = sum(abs(Signal).^2)/length(Signal);
+
+    % вычисление мощности шума
+    Pnoise = Psignal / (10 ^ (SNR / 10));
+    
+    % генерация белого шума
+%     fprintf('size(Signal) = %d', size(Signal));
+    sz = length(Signal);
+    Noise = normrnd(0, sqrt(Pnoise/2), [1, sz]) + 1i*normrnd(0, sqrt(Pnoise/2), [1, sz]);
+    
+    % сложение сигнала и шума
+    NoisedSignal = Signal + Noise;
+end
